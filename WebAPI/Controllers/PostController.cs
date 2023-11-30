95e38d5fbbfe03b1f7be6a24019f2dae87648da1
lillaknt <lillaknt@gmail.com>
2023-11-30 14:59:08 +0100

    Update Post Controller

--- WebAPI/Controllers/PostController.cs
+++ WebAPI/Controllers/PostController.cs
@@ -30,5 +30,66 @@ public class PostController : ControllerBase
             return StatusCode(500, e.Message);
         }
     }
+    
+    [HttpGet]
+    public async Task<ActionResult<IEnumerable<Post>>> GetAsync( [FromQuery] string? titleContains,[FromQuery] string? bodyContains)
+    {
+        try
+        {
+            SearchPostParametersDto parameters = new( titleContains, bodyContains);
+            var todos = await postLogic.GetAsync(parameters);
+            return Ok(todos);
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine(e);
+            return StatusCode(500, e.Message);
+        }
+    }
+    
+    [HttpPatch]
+    public async Task<ActionResult> UpdateAsync([FromBody] PostUpdateDto dto)
+    {
+        try
+        {
+            await postLogic.UpdateAsync(dto);
+            return Ok();
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine(e);
+            return StatusCode(500, e.Message);
+        }
+    }
+    
+    [HttpDelete("{id:int}")]
+    public async Task<ActionResult> DeleteAsync([FromRoute] int id)
+    {
+        try
+        {
+            await postLogic.DeleteAsync(id);
+            return Ok();
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine(e);
+            return StatusCode(500, e.Message);
+        }
+    }
+    
+    [HttpGet("{id:int}")]
+    public async Task<ActionResult<GetPostIdDto>> GetById([FromRoute] int id)
+    {
+        try
+        {
+            GetPostIdDto result = await postLogic.GetByIdAsync(id);
+            return Ok(result);
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine(e);
+            return StatusCode(500, e.Message);
+        }
+    }
 
 }
