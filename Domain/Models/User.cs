6ba87b5cba0600781da1740eae9978446f878e71
lillaknt <lillaknt@gmail.com>
2023-11-30 11:20:51 +0100

    Updated user domain

--- Domain/Models/User.cs
+++ Domain/Models/User.cs
@@ -2,5 +2,21 @@ namespace Domain.Models;
 
 public class User
 {
+    public int Id { get; set; }
+    public string Email { get; set; }
+    public string FirstName { get; set; }
+    public string Password { get; set; }
+    
+    public int PostCode { get; set; }
+
+    public User(string email, string? firstName, string? password, int postCode)
+    {
+        this.Email = email;
+        this.FirstName = firstName;
+        this.Password = password;
+        this.PostCode = postCode;
+    }
+    
+    public User(){}
     
 }
